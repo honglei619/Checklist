8ef334549084e99e49bc62731cdd7df8aa31bc96
honglei <honglei_cn@126.com>
2015-05-11 22:59:12 +0800

    Lesson 21
    为第一次运行添加默认列表，自动跳转到添加任务view

--- Checklist/DataModel.m
+++ Checklist/DataModel.m
@@ -7,12 +7,13 @@
 //
 
 #import "DataModel.h"
+#import "Checklist.h"
 
 @implementation DataModel
 
 -(void)registerDefault{
     
-    NSDictionary *dictionary =@{@"ChecklistIndex":@-1};
+    NSDictionary *dictionary =@{@"ChecklistIndex":@-1,@"FirstTime":@ YES};
     [[NSUserDefaults standardUserDefaults]registerDefaults:dictionary                   ];
     
 }
@@ -29,47 +30,23 @@
     if ((self = [super init])) {
         [self loadChecklists];
         [self registerDefault];
+        [self handleFirstTime];
     }
     return self;
 }
-/*
--(NSString *)documentsDirectory{
-    
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentationDirectory, NSUserDomainMask, YES);
-    NSString *documentsDirectory = [paths firstObject];
-    
-    NSLog(@"file path is %@",documentsDirectory);
-    return documentsDirectory;
-}
--(NSString *)dataFilePath{
-    
-    
-    return [[self documentsDirectory]stringByAppendingPathComponent:@"Checklists.plist"];
-    //return [[self documentsDirectory]stringByAppendingPathComponent:@"Checklists.plist"];
-}
 
--(void)saveChecklists{
-    NSMutableData *data = [[NSMutableData alloc]init];
-    NSKeyedArchiver *archiver = [[NSKeyedArchiver alloc]initForWritingWithMutableData:data];
-    [archiver encodeObject:self.lists forKey:@"Checklists"];
-    [archiver finishEncoding];
-    [data writeToFile:[self dataFilePath] atomically:YES];
-}
-
--(void)loadChecklists{
-    
-    NSString *path =[self dataFilePath];
-    if([[NSFileManager defaultManager]fileExistsAtPath:path]){
-        NSData *data = [[NSData alloc]initWithContentsOfFile:path];
-        NSKeyedUnarchiver *unarchiver = [[NSKeyedUnarchiver alloc]initForReadingWithData:data];
-        self.lists = [unarchiver decodeObjectForKey:@"Checklists"];
-        [unarchiver finishDecoding];
-    }else{
-        self.lists = [[NSMutableArray alloc]initWithCapacity:20];
+-(void)handleFirstTime{
+    BOOL firstTime = [[NSUserDefaults standardUserDefaults]boolForKey:@"FirstTime"];
+    if (firstTime) {
+        Checklist *checklist = [[Checklist alloc]init];
+        checklist.name  = @"List";
+        [self.lists addObject:checklist];
+        [self setIndexOfSelectChecklist:0];
+        [[NSUserDefaults standardUserDefaults]setBool:NO forKey:@"FirstTime"];
+        
     }
 }
 
- */
 
 -(NSString*)documentsDirectory{
     
